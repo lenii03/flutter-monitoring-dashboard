710d5679e5899d79e3356cb77725d65a756bd3a5
StarsHat18 <dzuhria03@gmail.com>
2026-02-24 08:25:35 +0700

    minor: menghapus komenta di dashboard_nav_bloc

--- lib/features/dashboard/persentation/bloc/dashboard_nav_bloc.dart
+++ lib/features/dashboard/persentation/bloc/dashboard_nav_bloc.dart
@@ -3,11 +3,9 @@ import 'dashboard_nav_event.dart';
 import 'dashboard_nav_state.dart';
 
 class DashboardNavBloc extends Bloc<DashboardNavEvent, DashboardNavState> {
-  // Panggil dengan nama parameternya: selectedIndex: 0
   DashboardNavBloc() : super(DashboardNavState(selectedIndex: 0)) {
     
     on<NavigateToMenu>((event, emit) {
-      // Panggil dengan nama parameternya: selectedIndex: event.index
       emit(DashboardNavState(selectedIndex: event.index));
     });
   }
